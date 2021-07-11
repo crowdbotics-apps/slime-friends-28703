f23909f5795819d1007fe38922868b109c6930b3
Crowdbotics <team@crowdbotics.com>
2021-07-11 00:44:34 +0000

    Initial commit.

---
+++ backend/slime_friends_28703/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for slime_friends_28703 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slime_friends_28703.settings')
+
+application = get_wsgi_application()
